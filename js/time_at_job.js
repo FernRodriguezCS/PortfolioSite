5601c4e12d27539a06b0bac580a65fd5ca3249ad
Ferny Rodriguez <FernRodriguez328@gmail.com>
2025-09-11 22:44:37 -0700

    started date file with current info and job info

---
+++ js/time_at_job.js
@@ -0,0 +1,16 @@
+var currentDate = new Date();
+var currentMonthIndex = currentDate.getMonth() + 1; 
+var currentYear = currentDate.getFullYear();
+
+const CNinfo = {
+    company: 'CodeNinjas',
+    month: 5,
+    year: 2025,
+};
+
+const FDinfo = {
+    company: 'FWD:Dynamics',
+    month: 7,
+    year: 2025
+};
+
